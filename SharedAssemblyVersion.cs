7d77f7e55e59a1a05a67af69d8b563bbd1d4b925
huayu <noricohuas@126.com>
2017-01-04 15:43:03 +0800

    change version

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.99fb222")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.2031ae9")]
 [assembly: System.Reflection.AssemblyVersion("1.2.0")]
 [assembly: System.Reflection.AssemblyFileVersion("1.2.0")]
 
